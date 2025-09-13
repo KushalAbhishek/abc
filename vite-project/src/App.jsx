db7c01035a04d039e57ec690f0972863c1bf8b8e
KushalAbhishek <abhishek120340@gmail.com>
2025-09-13 16:59:23 +0530

    react-fundamentals

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,34 +4,47 @@ import "./App.css"
 
   function App() {
     const [count,setCount]=useState(0)
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    
 
-    const handleIncrement=()=>{
-      setCount((prevCount)=>prevCount+1)
-      console.log(count)
-    }
-
-    const handleClick=(name)=>{
+    /* const handleClick=(name)=>{
       alert("button pressed by "+ name)
-    }
-
-    const handleSumbit=()=>{
-      alert("You have submitted");
-    }
+    } */
 
     const handleChange=(event)=>{
-      event.preventDefault(); //prevents the website from refreshing immediately
-      console.log(event.target.name);
+      //event.preventDefault(); //prevents the website from refreshing immediately
+      console.log(event.target.name);//for form
       console.log(event.target.value)
     }
 
+    const handleSumbit=()=>{
+      event.preventDefault();
+      alert(`Email: ${email} \nPassword: ${password} \nYou have submitted`);//for form
+    }
+
+    //to store the states of the enter text we use handleemail and handlepassword
+    const handleEmail=(event)=>{
+      setEmail(event.target.value)
+    }
+
+    const handlePassword=(event)=>{
+      setEmail(event.target.value)
+    }
+
+    /* const handleIncrement=()=>{
+      setCount((prevCount)=>prevCount+1)
+      console.log(count)
+    } */
+
     return (
       <div>
-        <p>{count}</p>
-        <button onClick={handleIncrement}>Increment</button>
+        {/* <p>{count}</p>
+        <button onClick={handleIncrement}>Increment</button> */}
         <form onSubmit={handleSumbit}>
-          <input type="text" name="email" placeholder="Enter email" onChange={handleChange}/>
+          <input type="text" name="email" placeholder="Enter email" onChange={handleEmail}/>
           <input type="password" name="password" placeholder="Enter password" 
-          onChange={handleChange}/>
+          onChange={handlePassword}/>
           <button type="submit">Login</button>
         </form> 
       </div>
