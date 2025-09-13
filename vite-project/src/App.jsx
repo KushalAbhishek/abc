bd3b9a042842f36fa882bb306fbefc9966943d87
KushalAbhishek <abhishek120340@gmail.com>
2025-09-13 15:27:03 +0530

    react-fundamentals

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,10 @@ import Profile from "./Profile.jsx"
 import ProductCard from "./ProductCard.jsx"
 import Greeting from "./Greeting.jsx"
 import "./App.css"
+import BlogPost from "./BlogPost.jsx"
 
   function App() {
+  /*
   const name = "Kushal";
   const age = 21 ;
   const isStudent = true ;
@@ -11,13 +13,81 @@ import "./App.css"
   const rollNo = "23VV1A0527";
   const booGr = "O+ve";
   const doB = "08/09/2004";
-
-  const productName = "Chocolate";
   
+  const productName = "Chocolate";
+  const price = 5000;
+  const isAvailable = true;
+  */
+
+  /* const products=[{
+    productName : "Chocolate",
+    price : 5000,
+    isAvailable : true
+  },{
+    productName : "Biscuit",
+    price : 6000,
+    isAvailable : true
+  },{
+    productName : "Honey",
+    price : 7000,
+    isAvailable : true
+  }] */
+  const posts=[
+    {
+      id:1,
+      author:"Kushal Abhishek",
+      title:"The Walking Dead",
+      description:"zombie book"
+    },
+    {
+      id:2,
+      author:"Karan Ashish",
+      title:"The art to attract",
+      description:"social skill"
+    },
+    {
+      id:3,
+      author:"Abhi",
+      title:"Cyberpunk",
+      description:"Distopian future"
+    }
+  ]
   
   return (
+    <div className="cart">
+      {
+      posts.map((post)=>(
+        <BlogPost
+        key={post.id}
+        author={post.author}
+        title={post.title}
+        description={post.description}
+        />
+      ))
+    }
+    </div>
+    
+    
+   /*  
+   <div className="maincontainer">
+        
+     {
+       products.map((product)=>(
+       <ProductCard
+       productName={product.productName}
+       price={product.price}
+       isAvailable={product.isAvailable}
+      />
+      ))
+  }
+        
+      </div>
+   */
+   /* 
     <div>
+
     <Profile />
+    
     <div className="cart">
       <div className="pro1">
         <ProductCard className="c1" recievedName={productName} price={5000} isAvailable={true}/>
@@ -35,7 +105,9 @@ import "./App.css"
     </div>
     
       
-    </div>
+    </div>*/
+    
+    
   )
 }
 
