e4ca90e1ccd1a7e59ba3f87436bde0776425e940
KushalAbhishek <abhishek120340@gmail.com>
2025-09-13 16:30:42 +0530

    react-fundamentals

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,39 @@
+import { useState } from "react"
 import "./App.css"
 
 
   function App() {
+    const [count,setCount]=useState(0)
+
+    const handleIncrement=()=>{
+      setCount((prevCount)=>prevCount+1)
+      console.log(count)
+    }
 
     const handleClick=(name)=>{
       alert("button pressed by "+ name)
     }
 
+    const handleSumbit=()=>{
+      alert("You have submitted");
+    }
+
     const handleChange=(event)=>{
+      event.preventDefault(); //prevents the website from refreshing immediately
       console.log(event.target.name);
       console.log(event.target.value)
     }
 
     return (
       <div>
-        <p>This is a Button</p>
-        <input type="text" name="email" placeholder="Enter email" onChange={handleChange}/>
-        <input type="password" name="password" placeholder="Enter password" 
-        onChange={handleChange}/>
-        <button onClick={()=>handleClick("Abhishek")}>Button</button>
+        <p>{count}</p>
+        <button onClick={handleIncrement}>Increment</button>
+        <form onSubmit={handleSumbit}>
+          <input type="text" name="email" placeholder="Enter email" onChange={handleChange}/>
+          <input type="password" name="password" placeholder="Enter password" 
+          onChange={handleChange}/>
+          <button type="submit">Login</button>
+        </form> 
       </div>
   
     )
